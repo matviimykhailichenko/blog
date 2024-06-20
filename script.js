4a286e1fa0211fb205e0c47d57d95b177252bc32
Matvii Mykhailichenko <103883982+matthewthefool@users.noreply.github.com>
2024-06-20 20:36:45 +0200

    dfg

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
+window.onload = function() {
+    // Trigger the About tab to open when the page loads
+    openTab(null, 'About');
+};
 document.addEventListener("DOMContentLoaded", function() {
     loadResearchData();
 });
 
 function openTab(evt, tabName) {
+    // Declare all variables
     var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
+
+    // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+    if (evt) {
+        evt.currentTarget.className += " active";
+    } else {
+        document.querySelector(`.tablinks[onclick="openTab(event, '${tabName}')"]`).className += " active";
+    }
 }
 
 function loadResearchData() {
